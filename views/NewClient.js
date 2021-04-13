ca222dae19190b21ab845c50a27d2d5e48647c77
francijpg <hellofrancijpg@gmail.com>
2021-04-13 14:20:50 -0400

    Create NewClient.js

---
+++ views/NewClient.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const NewClient = () => {
+  return (
+    <View>
+      <Text>NewClient</Text>
+    </View>
+  );
+};
+
+export default NewClient;
