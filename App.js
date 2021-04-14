b30c4b8b5c8e50eaea4ab5bc89d71f1ff1c75c13
francijpg <hellofrancijpg@gmail.com>
2021-04-14 08:43:20 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,72 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+
+import Home from './views/Home';
+import NewClient from './views/NewClient';
+import ClientDetail from './views/ClientDetail';
+import TopBar from './components/ui/TopBar';
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774F2',
+    accent: '#0655BF',
+  },
+};
+
+// console.log(theme);
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.colors.primary,
+            },
+            headerTintColor: theme.colors.surface,
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={({navigation, route}) => ({
+              headerTitleAlign: 'center',
+              headerLeft: props => (
+                <TopBar {...props} navigation={navigation} route={route} />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="NewClient"
+            component={NewClient}
+            options={{
+              title: 'New Client',
+            }}
+          />
+          <Stack.Screen
+            name="ClientDetail"
+            component={ClientDetail}
+            options={{
+              title: 'Client Details',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
