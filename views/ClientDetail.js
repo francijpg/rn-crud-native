86892cfb98b7b434faf1087513addb807444661a
francijpg <hellofrancijpg@gmail.com>
2021-04-15 17:10:11 -0400

    Update ClientDetail.js

--- views/ClientDetail.js
+++ views/ClientDetail.js
@@ -1,12 +1,79 @@
+import axios from 'axios';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Alert, StyleSheet, View} from 'react-native';
+import {Button, FAB, Headline, Subheading, Text} from 'react-native-paper';
+import globalStyles from '../styles/global';
 
-const ClientDetail = () => {
+const ClientDetail = ({navigation, route}) => {
+  const {setConsultAPI} = route.params;
+  const {name, phone, email, company, id} = route.params.item;
+
+  const showConfirmation = () => {
+    Alert.alert(
+      'Do you want to delete this client?',
+      'A contact will be deleted. This cannot be recovered',
+      [
+        {text: 'Yes, Delete', onPress: () => deleteClient()},
+        {text: 'Cancel', style: 'cancel'},
+      ],
+    );
+  };
+  const deleteClient = async () => {
+    const url = `http://localhost:8000/clients/${id}`;
+    // console.log(url);
+    try {
+      await axios.delete(url);
+    } catch (error) {
+      console.log(error);
+    }
+
+    navigation.navigate('Home');
+    setConsultAPI(true);
+  };
   return (
-    <View>
-      <Text>ClientDetail</Text>
+    <View style={globalStyles.container}>
+      <Headline style={globalStyles.title}>{name}</Headline>
+      <Text style={styles.text}>
+        Company: <Subheading>{company}</Subheading>{' '}
+      </Text>
+      <Text style={styles.text}>
+        Email: <Subheading>{email}</Subheading>{' '}
+      </Text>
+      <Text style={styles.text}>
+        Phone: <Subheading>{phone}</Subheading>{' '}
+      </Text>
+
+      <Button
+        style={styles.button}
+        mode="contained"
+        icon="cancel"
+        onPress={() => showConfirmation()}>
+        Delete Client
+      </Button>
+
+      <FAB
+        icon="pencil"
+        style={globalStyles.fab}
+        onPress={() =>
+          navigation.navigate('NewClient', {
+            client: route.params.item,
+            setConsultAPI,
+          })
+        }
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    marginBottom: 20,
+    fontSize: 18,
+  },
+  button: {
+    marginTop: 100,
+    backgroundColor: 'red',
+  },
+});
 
 export default ClientDetail;
